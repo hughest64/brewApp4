2129138b9a1ab6615a1824a36c68ed0d290a21f5
hughest64 <hughest64@gmail.com>
2017-03-09 21:10:59 -0600

    Add files via upload
    Added HH functionality for setting hours to the timer, renamed functions and added some notes for future updates

--- BrewWeb/static/timer.js
+++ BrewWeb/static/timer.js
@@ -1,55 +1,54 @@
-/*TODO:
-- change fields to HH and MM (no real need to set seconds)
-- test
-*/
-
+var hr = document.getElementById('hr')
 var mn = document.getElementById('mn');
 var sec = document.getElementById('sec');
 var display = document.getElementById('timer');
 var isRunning = false;
 
-var totalAdds = 0;
 
 function setTimer() {
     // we only set the timer if there is some value to set
-    if (mn.value || sec.value) {
+    if (hr.value || mn.value || sec.value) {
         // the 450 adds a bump to ms and keeps the timer from skipping
         var remainingTime = (
-            (Math.abs(mn.value) * 60000) + (Math.abs(sec.value * 1000)) + 30
+            (Math.abs(Math.floor(hr.value)) * 3600000) +
+            (Math.abs(Math.floor(mn.value)) * 60000) +
+            (Math.abs(Math.floor(sec.value * 1000))) + 450
         );
         var resetTime = remainingTime;
         localStorage['remainingTime'] = remainingTime;
         localStorage['resetTime'] = resetTime;
-        format(remainingTime);
+        formatDisplay(remainingTime);
+    } else {
+        console.log('this shit does not work')
     }
 }
 
-function run() {
+function runTimer() {
     if (isRunning) {
         var endTime = Number(localStorage['endTime']);
         var remainingTime = endTime - Date.now();
         // adding some buffer time to make sure we don't lose any time
         if (remainingTime % 1000 < 10) {
-            endTime += 451;
+            endTime += 441;
             localStorage['endTime'] = endTime;
         }
         localStorage['remainingTime'] = remainingTime;
-        format(remainingTime);
+        formatDisplay(remainingTime);
         if (remainingTime > 999) {
-            timer = setTimeout(run, 1000);
+            timer = setTimeout(runTimer, 1000);
         } else {
+            // make values falsey
             isRunning = false;
-            // make 'remainingTime', and 'resetTime' falsey
             localStorage['remainingTime'] = '';
             localStorage['resetTime'] = '';
-            // tests remove later
-            var dif = Number(OGend)
-            var now = Date.now()
+            // I think we add a check to see if there is a next step
+            // if there is, we set the timer with that value
+            // I think we need to json an array in local storage
         }
     }
 }
 
-function format(t) {
+function formatDisplay(t) {
     var h = Math.floor(t/3600000);
     var m = Math.floor(t/60000) % 60;
     var s = Math.floor(t/1000) % 60;
@@ -65,28 +64,35 @@ function format(t) {
     }
 }
 
-function start() {
+function startTimer() {
     if (!isRunning && localStorage['remainingTime']) {
-        isRunning = true;
+        isRunning= true;
         var remainingTime = Number(localStorage['remainingTime'])
         var endTime = Date.now() + remainingTime;
-        // just for a test, remove later
-        OGend = endTime;
         localStorage['endTime'] = endTime;
-        run();
+        runTimer();
     }
 }
 
-function stop() {
+function stopTimer() {
     isRunning = false;
     clearTimeout(timer);
 }
 
-function reset() {
-    stop();
+function resetTimer() {
+    stopTimer();
     var remainingTime = localStorage['resetTime'];
     localStorage['remainingTime'] = remainingTime;
-    format(remainingTime);
+    formatDisplay(remainingTime);
 }
 
-start();
+function clearTimer() {
+    stopTimer();
+    isRunning = false;
+    localStorage['remainingTime'] = '';
+    localStorage['resetTime'] = '';
+    localStorage['endTime'] = '';
+    var display = document.getElementById('timer').innerHTML = '00:00';
+}
+
+startTimer();
